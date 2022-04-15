05b47db3f293503d4a6f5504b6e3c645cb4dceff
Matthew Wiser <matt.wiser@icloud.com>
2022-04-14 20:52:38 -0500

    refactored tests and added more

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,16 +8,42 @@ test("creates a new employee object", () => {
     }
     
     const employee = new Employee(data);
+
+    expect(employee).toEqual(expect.any(Object));
+});
+
+test("tests privacy of employee object attributes", () => {
+    const data = {
+        name: 'Dave',
+        email: 'dave@testcompany.com',
+        id: '12345'
+    }
     
+    const employee = new Employee(data);
+
     expect(employee.name).toBe(undefined);
-    expect(employee.getName()).toEqual('Dave');
    
     expect(employee.email).toBe(undefined);
-    expect(employee.getEmail()).toEqual('dave@testcompany.com');
    
     expect(employee.id).toBe(undefined);
-    expect(employee.getId()).toEqual('12345');
    
     expect(employee.role).toBe(undefined);
+});
+
+test("tests public data methods of employee objects", () => {
+    const data = {
+        name: 'Dave',
+        email: 'dave@testcompany.com',
+        id: '12345'
+    }
+    
+    const employee = new Employee(data);
+
+    expect(employee.getName()).toEqual('Dave');
+   
+    expect(employee.getEmail()).toEqual('dave@testcompany.com');
+   
+    expect(employee.getId()).toEqual('12345');
+   
     expect(employee.getRole()).toBe('Employee');
 });
